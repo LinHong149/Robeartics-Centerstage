adc0b4057e0a6f85ffb4d995928a568d56315df3
LinHong <linlin.hong1@gmail.com>
2024-02-08 10:23:03 -0500

    TensorFlow tflite asset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/auton/TensorFlowTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/auton/TensorFlowTesting.java
@@ -56,17 +56,13 @@ public class TensorFlowTesting extends LinearOpMode {
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "pixelStack.tflite";
+    private static final String TFOD_MODEL_ASSET = "PixelRecognition.tflite";
 //    // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
 //    // this is used when uploading models directly to the RC using the model upload interface.
     private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/pixel_quantized.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
-            "FIVE",
-            "FOUR",
-            "THREE",
-            "TWO",
-            "ONE",
+            "PIXELS",
             "KNOCKED"
     };
 
